a150e18294aad241d69de755cd987d5a12ca4ce1
William Arnold <mail@williamarnold.info>
2025-01-05 11:24:21 -0500

    Add some basic command-line programs.

--- home.nix
+++ home.nix
@@ -3,7 +3,15 @@
 {
     home = {
         packages = with pkgs; [
-            hello
+            # Enter list of packages to install here
+            
+            # Command-Line Utilities
+            zsh # Better shell
+            neovim # Better editor
+            bat zoxide # Better cat; Better cd
+            fzf fzf-zsh # Fuzzyfinder for zoxide; Fuzzyfinder for Zsh
+            
+            fastfetch # Better neofetch
         ];
 
         # Ensure this is set to correct username and home directory
